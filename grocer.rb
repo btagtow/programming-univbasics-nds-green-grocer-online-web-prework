7482af8668113be112f12619e269ba081fb8ff77
Ben Tagtow <bentagtow@gmail.com>
2019-11-15 00:33:09 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -39,7 +39,8 @@ def apply_coupons(cart:[], coupons:[])
     coupons.each do |coupon|
       if my_item == coupon[:item] && info[:count] >= [:num]
         info[:count] = info[:count] - coupon[:num]
-        if result["#{food}" W/COUPON]
+        if result["#{food} W/COUPON"]
+          
   
 #   index = 0 
 #   new_array = []
